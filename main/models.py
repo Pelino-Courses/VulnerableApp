b53dfcccf57b5f80508f720948305a51660fd1c1
niyo1999 <hassanniyo01@gmail.com>
2023-12-18 20:44:24 +0200

    nassan push

--- main/models.py
+++ main/models.py
@@ -1,7 +1,10 @@
-# main/models.py
-
 from django.db import models
 
-
 class ExampleModel(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, help_text="Enter a secure name")
+
+    def save(self, *args, **kwargs):
+        
+        self.name = self.name.strip()  
+        super().save(*args, **kwargs)
+
